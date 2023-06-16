5ba4aaa27870a8e6899b6d814c18522095519d51
Lisa Zhang <lpz2@duke.edu>
2023-06-16 14:19:17 -0400

    merging data table

--- app/main.py
+++ app/main.py
@@ -6,7 +6,17 @@ from typing import List, Union
 
 class Data(BaseModel):
     data_source: str
-    poc: Union[str, None]
+    platform: str
+    office: str
+    poc: str
+    app_auth: str
+    sensitivity: Union[str,None]
+    req_proc: bool
+    req_form: bool
+    app_req: str
+    provided: Union[str, None]
+    freeq: Union[str, None]
+    notes: str
 
 
 app = FastAPI()
@@ -21,11 +31,23 @@ cur = connection.cursor()
 
 @app.get('/source-poc')
 async def get_poc() -> List[Data]:
-    cur.execute("SELECT data_source, poc FROM di")
+    cur.execute("SELECT data_source,platform,office,poc,app_auth,sensitivity,req_proc,req_form,app_req,provided,freeq,notes FROM datainv")
+    # poc,
     rows = cur.fetchall()
     return [{
         "data_source": row[0],
-        "poc": row[1]
+        "platform": row[1],
+        "office": row[2].replace("{","").replace("}",""),
+        "poc": (str(row[3]).replace("[","").replace("]","").replace("'","")),
+        "app_auth": row[4].replace("{","").replace("}",""),
+        "sensitivity": row[5].replace("{", "").replace("}",""),
+        "req_proc": row[7],
+        "req_form": row[6],
+        "app_req": (row[8]),
+        "provided": row[9].replace("{", "").replace("}", ""),
+        "freeq": (row[10]).replace("{", "").replace("}",""),
+        "notes": (row[11])
+
     } for row in rows]
 
 
--- app/poc.js
+++ app/poc.js
@@ -9,42 +9,64 @@ export default function PersonContact() {
             .then((res) => res.json())
             .then((data) => {
                 setDatabases(data)
+               
             })
     }, [])
-    //   const personContactData = fetch('/api/source-poc/').then((res) => res.json())
-    //   let personContactList = [];
-    //   personContactData.then((databases) => {
-    //     for (let i in databases) {
-    //         console.log(databases[i].data_source)
-    //         personContactList.push(
-    //             <tr>
-    //                 <td>{databases[i].data_source}</td>
-    //                 <td>{databases[i].poc}</td>
-    //             </tr>
-    //         )
-    //     }
-    //   })
-    //   return personContactList;
-  
+
+    function printArray(arrayString) {
+      if ( arrayString == "" | arrayString == " " | arrayString == "{}" | arrayString == null){
+        return "\n"
+      }
+      let toArray = arrayString.split(",")
+      let string = ""
+      for(let i = 0; i < toArray.length; i++){
+        string += toArray[i] + "\n"
+      }
+      return <td>{string}</td>
+    }
+ 
     return (
       <div>
         <h1>Who to Contact</h1>
         <table>
           <thead>
             <tr>
-              <th>Data Source</th>
-              <th>Person of Contact</th>
+            <th>Data Source</th>
+              <th>Platform</th>
+              <th>Office</th>
+             <th>Person of Contact</th>
+              <th>Approving Authority</th>
+              <th>Sensitivity</th>
+             <th>Access Request Form?</th>
+              <th>Access Request Process?</th>
+              <th>Approvals Requred?</th>
+              <th>How is Data Provided?</th>
+              <th>How Often Provided?</th>
+              <th>Notes</th> 
             </tr>
           </thead>
           <tbody>
-            {databases.map(({ data_source, poc }) => 
+            {databases.map(({data_source, platform,office,poc,app_auth, sensitivity,req_proc,req_form, app_req, provided, freeq, notes}) => 
+            //  poc, app_auth, sensitivity,req_proc,req_form, app_req, provided, freeq, notes
+             
               <tr>
                 <td>{data_source}</td>
-                <td>{poc}</td>
+                {printArray(platform)}
+                {printArray(office)}
+                {printArray(poc)}
+                {printArray(app_auth)}
+                {printArray(sensitivity)}
+                {req_form?<td>Yes</td>:<td>No</td>}
+                {req_proc?<td>Yes</td>:<td>No</td>}
+                {printArray(app_req)}
+                {printArray(provided)}
+                {printArray(freeq)}
+                {printArray(notes)}
+
               </tr>)
             }
           </tbody>
         </table>
       </div>
     );
-} 
+}
