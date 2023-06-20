c4c70f0d27e8a0ef99797adc338062dd0ba698e4
gummydumpling-2001 <69725888+gummydumpling-2001@users.noreply.github.com>
2023-06-20 10:36:00 -0400

    fun little commit

--- api/main.py
+++ api/main.py
@@ -3,7 +3,7 @@ import psycopg2
 from pydantic import BaseModel
 from typing import List, Union
 
-#Fields is a shortcut "map" so we dont have to type everything out if we add new filters
+# Fields is a shortcut "map" so we dont have to type everything out if we add new filters
 FIELDS = {
     "sensitivity": "ANY(sensitivity) = "
 }
@@ -20,7 +20,8 @@ class Data(BaseModel):
     sensitivity: Union[str, None]
     req_proc: str
 
-##app connects to fastapi so is why we say main:app when running uvicorn
+
+# app connects to fastapi so is why we say main:app when running uvicorn
 app = FastAPI()
 
 connection = psycopg2.connect(database="data_foundry",
@@ -31,10 +32,10 @@ connection = psycopg2.connect(database="data_foundry",
 cur = connection.cursor()
 
 
-##This is our query that sends back info from the database
+# This is our query that sends back info from the database
 @app.post('/data-table')
 async def get_data(filters: Filter) -> List[Data]:
-    ## Query is a string we concatenate to use multi lines 
+    # Query is a string we concatenate to use multi lines 
     query = f"SELECT data_source, poc, sensitivity, req_proc FROM datainv \
             WHERE '{filters.sensitivity}' = ANY(sensitivity) \
             AND req_proc = '{filters.request_process}';"
