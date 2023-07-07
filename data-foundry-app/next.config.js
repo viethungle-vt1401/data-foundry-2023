71159ba9de3eb5463996cb443b95f48744398a7c
gummydumpling-2001 <huynhphamphuongmai@gmail.com>
2023-07-07 15:05:06 -0400

    Initialized dockerize branch

---
+++ data-foundry-app/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = {  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:8000/:path*'
+      }
+    ]
+  }
+}
